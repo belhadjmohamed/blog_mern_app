23c967c8dfa860bee198e339417c44ad18ced966
Mohamed belhadj <110824757+belhadjmohamed@users.noreply.github.com>
2023-11-21 14:36:29 +0100

    Update post.js

--- client/src/post.js
+++ client/src/post.js
@@ -6,7 +6,7 @@ export default function Post({_id,title,summary,cover,content,createdAt,author})
       <div className="post">
         <div className="image">
           <Link to={'/post/'+_id}>
-            <img src={'https://blog-mern-app-api.vercel.app/'+cover} alt='' />
+            <img src={'http://localhost:4000/'+cover} alt='' />
           </Link>
         </div>
         <div className='texts'>
