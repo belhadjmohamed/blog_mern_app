a34d6daef281982918dbf046ff5960187f24c415
Mohamed belhadj <110824757+belhadjmohamed@users.noreply.github.com>
2023-11-21 14:26:18 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,8 +8,8 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const multer = require('multer');
-const uploadMiddleware = multer({dest: 'uploads/'});
-const fs = require('fs');
+//const uploadMiddleware = multer({dest: 'uploads/'});
+//const fs = require('fs');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'lfsdjfsjdpfjsofpoefpzoefjmjmm';
