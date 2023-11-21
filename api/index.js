59f419e2aa4cb5c4d7aa0d1c75a6ad8a1fc66314
Mohamed belhadj <110824757+belhadjmohamed@users.noreply.github.com>
2023-11-21 14:29:04 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,8 +8,8 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 const multer = require('multer');
-//const uploadMiddleware = multer({dest: 'uploads/'});
-//const fs = require('fs');
+const uploadMiddleware = multer({dest: 'uploads/'});
+const fs = require('fs');
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'lfsdjfsjdpfjsofpoefpzoefjmjmm';
@@ -75,12 +75,12 @@ app.post('/logout', (req,res) => {
     res.cookie('token','').json('ok');
 });
 
-app.post('/post', uploadMiddleware.single('file'), async (req,res)=>{
+//app.post('/post', uploadMiddleware.single('file'), async (req,res)=>{
     const {originalname,path} = req.file;
     const parts = originalname.split('.');
     const ext = parts[parts.length - 1];
     const newPath =  path+'.'+ext;
-    //fs.renameSync(path,newPath);
+    fs.renameSync(path,newPath);
     
     const {token} = req.cookies;
     jwt.verify(token,secret,{}, async (err,info)=>{
@@ -106,7 +106,7 @@ app.get('/post', async (req,res) => {
     );
 });
 
-app.put('/post', uploadMiddleware.single('file') ,async (req,res) => {
+//app.put('/post', uploadMiddleware.single('file') ,async (req,res) => {
     let newPath =  null;
     if (req.file) {
         const {originalname,path} = req.file;
