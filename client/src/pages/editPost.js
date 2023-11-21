8336d6f8e4304abd4e9333b9d0bf57106c4a097a
Mohamed belhadj <110824757+belhadjmohamed@users.noreply.github.com>
2023-11-21 14:39:29 +0100

    Update editPost.js

--- client/src/pages/editPost.js
+++ client/src/pages/editPost.js
@@ -12,7 +12,7 @@ export default function EditPost(){
     const [redirect , setRedirect] = useState(false);
 
     useEffect (()=> {
-        fetch('https://blog-mern-app-api.vercel.app/post/'+id)
+        fetch('http://localhost:4000/'+id)
         .then(response => {
             response.json().then(postInfo => {
                 setTitle(postInfo.title);
