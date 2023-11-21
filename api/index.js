8143afea0b0eaf25af4e70704ed8691cb1bb8612
Mohamed belhadj <110824757+belhadjmohamed@users.noreply.github.com>
2023-11-21 14:24:48 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -80,7 +80,7 @@ app.post('/post', uploadMiddleware.single('file'), async (req,res)=>{
     const parts = originalname.split('.');
     const ext = parts[parts.length - 1];
     const newPath =  path+'.'+ext;
-    fs.renameSync(path,newPath);
+    //fs.renameSync(path,newPath);
     
     const {token} = req.cookies;
     jwt.verify(token,secret,{}, async (err,info)=>{
